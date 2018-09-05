a7c9b58b48bcf4c800620f428f75d147fd7f3c91
H. Kim <38238305+hachijo31@users.noreply.github.com>
2018-09-05 13:29:55 +0900

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# written by Hyunjoon Kim
+import csv
+import os
+
+def filepath(year, month, city='nyc', data_dir='../data'):
+    """
+    return filepath
+    """
+    if city=='nyc' :
+        return os.path.join(data_dir, filename_nyc(year, month))
+    else:
+        raise ValueError("Undefined City! : ", city)
+
+def filename_nyc(year, month):
+    """
+    input:
+        year, month : int
+    return:
+        string
+    ~ 2014-08
+    "YYYY-MM - Citi Bike trip data.csv"
+    2014-09 ~ 2017-12 
+    "YYYYMM-citibike-tripdata.csv"
+    2018-01 ~ 2018-03 
+    "YYYYMM-citibikenyc_tripdata.csv"
+    2018-04 ~
+    "YYYYMM-citibike-tripdata.csv"
+    """
+    if int(year) < 2014 or ((int(year) == 2014) and (int(month)<=8)) :
+        filename = "{0:04d}-{1:02d} - Citi Bike trip data.csv".format(year, month) 
+    elif year == 2018 and month <= 3 :
+        filename = "{0:04d}{1:02d}-citibikenyc_tripdata.csv".format(year, month) 
+    else:
+        filename = "{0:04d}{1:02d}-citibike-tripdata.csv".format(year, month)
+    return filename
+
+def make_tripduration_list(year, month):
+    with open(filepath(year, month)) as csvfile:
+        reader = csv.DictReader(csvfile)
+        td = []
+        for row in reader:
+            td.append(int(row["tripduration"]))
+    print(td[0:6])
+
+if __name__ == "__main__":
+    print(filename_nyc(2012, 7))
+    print(filename_nyc(2014, 8))
+    print(filename_nyc(2018, 1))
+    print(filename_nyc(2018, 7))
+    print(filepath(2012, 7))
+    print(filepath(2014, 8))
+    print(filepath(2018, 1))
+    print(filepath(2018, 7))
+    make_tripduration_list(2016, 1)
