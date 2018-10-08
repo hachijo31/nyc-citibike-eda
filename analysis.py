bcc16b1418f7f77967e150d02a3957dec52db6c0
H. Kim <38238305+hachijo31@users.noreply.github.com>
2018-10-08 20:30:08 +0900

    Chage date format to datetime.date.toordinal, fix code to PEP8

--- analysis.py
+++ analysis.py
@@ -2,18 +2,22 @@
 # written by Hyunjoon Kim
 import csv
 import os
+import datetime as dt
 import matplotlib
-matplotlib.use('Agg') #for cli
+#for cli
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 def filepath(year, month, city='nyc', data_dir='data'):
     """
     return filepath
     """
-    if city=='nyc' :
+    if city == 'nyc':
         return os.path.join(data_dir, filename_nyc(year, month))
     else:
         raise ValueError("Undefined City! : ", city)
+
 
 def filename_nyc(year, month):
     """
@@ -23,14 +27,14 @@ def filename_nyc(year, month):
         string
     ~ 2014-08
     "YYYY-MM - Citi Bike trip data.csv"
-    2014-09 ~ 2017-12 
+    2014-09 ~ 2017-12
     "YYYYMM-citibike-tripdata.csv"
-    2018-01 ~ 2018-03 
+    2018-01 ~ 2018-03
     "YYYYMM-citibikenyc_tripdata.csv"
     2018-04 ~
     "YYYYMM-citibike-tripdata.csv"
     """
-    if int(year) < 2014 or ((int(year) == 2014) and (int(month)<=8)) :
+    if int(year) < 2014 or ((int(year) == 2014) and (int(month) <= 8)):
         filename = "{0:04d}-{1:02d} - Citi Bike trip data.csv".format(year, month)
     #elif year == 2018 and month <= 3 :
         #filename = "{0:04d}{1:02d}_citibikenyc_tripdata.csv".format(year, month)
@@ -38,12 +42,15 @@ def filename_nyc(year, month):
         filename = "{0:04d}{1:02d}-citibike-tripdata.csv".format(year, month)
     return filename
 
+
 def refresh_station_dict(station_id, name, lat, lon, year, month, station_dict):
+    latest_use_date = dt.date(year, month, 1).toordinal()
     if station_id in station_dict:
-        station_dict[station_id][5] = year*12 + month - 1
+        station_dict[station_id][5] = latest_use_date
     else:
-        station_dict[station_id] = [station_id, name, lat, lon, year*12 + month - 1, year*12 + month - 1]
+        station_dict[station_id] = [station_id, name, lat, lon, latest_use_date, latest_use_date]
     return station_dict
+
 
 def append_station_dict(year, month, station_dict):
     with open(filepath(year, month)) as csvfile:
@@ -54,21 +61,31 @@ def append_station_dict(year, month, station_dict):
             station_dict = refresh_station_dict(row[7], row[8], row[9], row[10], year, month, station_dict)
     return station_dict
 
+
 def make_station_dict(start_year, start_month, end_year, end_month):
     station_dict = {}
-    for tmp in range(start_year*12 + start_month - 1, end_year*12 + end_month - 1):
-        print("append_station_dict {}-{}".format(tmp//12, tmp % 12 + 1))
-        station_dict = append_station_dict(tmp//12, tmp % 12 + 1, station_dict)
+    start = dt.date(start_year, start_month, 1)
+    end = dt.date(end_year, end_month, 1)
+    tmp = start
+    while tmp < end:
+        print("append_station_dict {}-{}".format(tmp.year, tmp.month))
+        station_dict = append_station_dict(tmp.year, tmp.month, station_dict)
+        if tmp.month == 12:
+            tmp = dt.date(tmp.year + 1, 1, 1)
+        else:
+            tmp = dt.date(tmp.year, tmp.month + 1, 1)
     return station_dict
+
 
 def save_station_dict_csv(station_dict):
     with open("station_info.csv", "w") as f:
         fn = ['station id', 'station name', 'station latitude', 'station longitude', 'first usage', 'lastest usage']
-        writer = csv.DictWriter(f, fieldnames = fn)
+        writer = csv.DictWriter(f, fieldnames=fn)
         writer.writeheader()
         for i, v in station_dict.items():
             print(v)
-            writer.writerow({fn[0]:v[0], fn[1]:v[1], fn[2]:v[2], fn[3]:v[3], fn[4]:v[4], fn[5]:v[5]})
+            writer.writerow({fn[0]: v[0], fn[1]: v[1], fn[2]: v[2], fn[3]: v[3], fn[4]: v[4], fn[5]: v[5]})
+
 
 def make_tripduration_list(year, month):
     with open(filepath(year, month)) as csvfile:
@@ -90,17 +107,10 @@ def make_tripduration_list(year, month):
     '''
     return td
 
-"""
-def plot_tripduration(td_list, filename):
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.hist(td_list, bins=60, range=(0, 3000))
-    plt.savefig(filename)
-"""
 
 def plot_month_tripduration(year, month):
-    td=make_tripduration_list(year, month)
-    filename='tripduration_{0:d}{1:02d}.png'.format(year, month)
+    td = make_tripduration_list(year, month)
+    filename = 'tripduration_{0:d}{1:02d}.png'.format(year, month)
 
     fig = plt.figure()
     plt.title("Trip distribution {0:d}-{1:02d}".format(year, month))
@@ -113,18 +123,11 @@ def plot_month_tripduration(year, month):
     print(" End to plot '{}'".format(filename))
 
 if __name__ == "__main__":
-    print(filename_nyc(2012, 7))
-    print(filename_nyc(2014, 8))
-    print(filename_nyc(2018, 1))
-    print(filename_nyc(2018, 7))
-    print(filepath(2012, 7))
-    print(filepath(2014, 8))
-    print(filepath(2018, 1))
-    print(filepath(2018, 7))
-    station_dict = make_station_dict(2013, 7, 2018, 9)
+    station_dict = make_station_dict(2013, 7, 2014, 9)
+    print("aaa")
     save_station_dict_csv(station_dict)
-    '''
+    """
     for j in range(2015, 2017):
         for i in range(12):
             plot_month_tripduration(j, i+1)
-    '''
+    """
